906f87676648ba2c9292cfe436bfb488e7fc5dfa
WilhelmWeber <kamiobana2345@gmail.com>
2024-02-17 17:45:47 +0900

    first commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name="FastIIIFimageAPI",
+    description="iiif ImageAPI for FastAPI",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/WilhelmWeber",
+    license='MIT',
+    author='Yuto_Takizawa',
+    author_email="mokoda5243@gmail.com",
+    version='0.0.1',
+    package_dir=['./dist'],
+    install_requires={
+        'pillow>=10.2.0',
+    },
+)
