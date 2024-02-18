241d3433d819185c8c9b5684cc76c0c3ffe6b255
WilhelmWeber <kamiobana2345@gmail.com>
2024-02-18 18:42:12 +0900

    major changes

--- setup.py
+++ setup.py
@@ -1,3 +1,7 @@
+# Author: Yuto Takizawa <mokoda5243@gmail.com>
+# Copyright © 2024- Yuto Takizawa
+# Licence: MIT
+
 from setuptools import setup
 
 setup(
@@ -5,14 +9,20 @@ setup(
     description="iiif ImageAPI for FastAPI",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url="https://github.com/WilhelmWeber",
+    url="https://github.com/WilhelmWeber/IIIFimageAPIforFastAPI",
     license='MIT',
+    download_url="https://github.com/WilhelmWeber/IIIFimageAPIforFastAPI",
     author='Yuto_Takizawa',
     author_email="mokoda5243@gmail.com",
     version='0.0.1',
-    package_dir=['./dist'],
-    install_requires={
+    packages=['iafa'],
+    keywords='iiif fastapi',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+    ],
+    install_requires=[
         'pillow>=10.2.0',
-        'fastapi>=0.104.1'
-    },
+        'fastapi>=0.104.1',
+    ],
 )
